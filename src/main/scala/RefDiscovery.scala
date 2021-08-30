14454977520d62d9bf1bb84608e0e3baff6c8cf6
aryzach <zach_asmith@yahoo.com>
2021-08-29 21:20:49 -0400

    passing pktLine test

--- src/main/scala/RefDiscovery.scala
+++ src/main/scala/RefDiscovery.scala
@@ -8,7 +8,10 @@ pktLine msg = printf "%04s%s" (toHex . (4 +) $ length msg) msg
 */
 
 object RefDiscovery extends App {
-  def pktLine(msg: String): String = msg
+  def pktLine(msg: String): String = {
+    val hex = (4 + msg.length)
+    f"$hex%04x$msg%s" //(toHex . (4 +) $ length msg) msg
+  }
 
   def gitProtoRequest(host: String)(repo: String): String = host
 }
